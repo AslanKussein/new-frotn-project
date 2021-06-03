42a6968521ce344c2ba64e301951a94e95a1c5c6
Aslan <kussein93@gmail.com>
2021-06-04 03:52:05 +0600

    das

--- frontend/src/app/service/config.service.ts
+++ frontend/src/app/service/config.service.ts
@@ -5,9 +5,12 @@ import {Injectable} from '@angular/core';
 })
 export class ConfigService {
   // serverAddress = window.location.origin;
-  apiUrl = '/sso/api';
-  openApiUrl = '/sso/open-api';
-  authUrl = '/sso/open-api/auth';
+  // apiUrl = '/sso/api';
+  // openApiUrl = '/sso/open-api';
+  // authUrl = '/sso/open-api/auth';
+  apiUrl = 'http://localhost:8080/sso/api';
+  openApiUrl = 'http://localhost:8080/sso/open-api';
+  authUrl = 'http://localhost:8080/sso/open-api/auth';
 
   constructor() {
   }
